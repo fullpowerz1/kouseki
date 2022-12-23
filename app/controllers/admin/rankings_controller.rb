ff638a576eee36757e894791d5c1ec4c3f8ffe9a
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2022-12-23 08:02:01 +0000

    hara

--- app/controllers/admin/rankings_controller.rb
+++ app/controllers/admin/rankings_controller.rb
@@ -1,2 +1,18 @@
 class Admin::RankingsController < ApplicationController
+
+  def show
+    @ranking = Ranking.find(params[:id])
+  end
+
+  def update
+    @ranking = Ranking.find(params[:id])
+    @ranking.update(ranking_params)
+    redirect_to admin_ranking_path(@ranking)
+  end
+
+  private
+
+  def ranking_params
+    params.require(:ranking).permit(:customer_id, :ore, :type, :name, :amount, :rank, :status)
+  end
 end
--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -6,6 +6,10 @@ class Ranking < ApplicationRecord
       created_at.strftime("%Y/%m/%d %H:%M:%S")
     end
 
+    def full_name
+    self.first_name + self.last_name
+    end
+
     enum rank: { rank1: 0, rank2: 1, rank3: 2 }
 
 end
