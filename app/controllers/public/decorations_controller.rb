b640449cb1209c4da5d2978b18635ed3c1ba59da
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2022-12-23 01:21:51 +0000

    homepage

--- app/controllers/public/decorations_controller.rb
+++ app/controllers/public/decorations_controller.rb
@@ -1,2 +1,47 @@
 class Public::DecorationsController < ApplicationController
+  def index
+    @decorations = current_customer.decorations
+    @decoration = Decoration.new
+    @current_customer = current_customer
+  end
+
+  def edit
+    @decoration = Decoration.find(params[:id])
+    @current_customer = current_customer
+  end
+
+  def create
+    @decoration = Decoration.new(decoration_params)
+    if @decoration.save
+      redirect_to request.referer
+    else
+      @decorations = Decoration.all
+      @current_customer = current_customer
+      render :index
+    end
+  end
+
+  def update
+    @decoration = Decoration.find(params[:id])
+    if @decoration.update(decoration_params)
+      redirect_to decorations_path(@decoration.id)
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @decoration = Decoration.find(params[:id])
+    @decoration.destroy
+    redirect_to decorations_path
+  end
+
+  private
+
+  def decoration_params
+    params.require(:decoration).permit(:customer_id, :ore, :type, :nickname)
+  end
+
+
 end
--- app/controllers/public/rankings_controller.rb
+++ app/controllers/public/rankings_controller.rb
@@ -7,6 +7,26 @@ class Public::RankingsController < ApplicationController
 
   def confirm
     @ranking = Ranking.new(ranking_params)
+    @ranking.rank = params[:ranking][:rank] ##順位付けを反映
+
+    # 手持ちの鉱石から選択
+    if params[:ranking][:select_material] == "0" ##どれを選択したのかをparamsから読み取る。
+      @ranking.ore = current_customer.ore ##ランク付けする鉱石を反映
+      @ranking.amount =current_customer.amount ##顧客がランク付けしたい鉱石の数
+      render "confirm"
+
+    # 装飾品を選択
+    elsif params[:ranking][:select_material] == "1" ##どれを選択したのかをparamsから読み取る。
+      @decoration = current_customer.decorations.new ##装飾品を新規登録
+      @decoration.type = [:ranking][:type] #装飾品の種類を登録
+      @decoration.customer_id = current_customer.id ##装飾品カスタマーIDをログインしているゲストIDに指定
+
+      if @decoration.save
+        @ranking.type = @decoration.type
+      else
+        render "new"
+      end
+    end
   end
 
   def complete
@@ -14,6 +34,7 @@ class Public::RankingsController < ApplicationController
 
   def create
     @ranking = Ranking.new(ranking_params)
+    @ranking.customer_id = current_customer.id
     @ranking.save!
     redirect_to rankings_complete_path
   end
@@ -29,4 +50,5 @@ class Public::RankingsController < ApplicationController
   def ranking_params
     params.require(:ranking).permit(:customer_id, :ore, :type, :name, :amount, :rank, :status)
   end
+
 end
