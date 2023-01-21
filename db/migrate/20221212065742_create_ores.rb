71bb72c1945ccbbe8ba2f9b1b351c608079bb771
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2023-01-21 08:21:34 +0000

    pikari

--- db/migrate/20221212065742_create_ores.rb
+++ db/migrate/20221212065742_create_ores.rb
@@ -4,7 +4,7 @@ class CreateOres < ActiveRecord::Migration[6.1]
      t.integer :genre_id,              null: false
      t.string :stone,                    null: false, default: ""
      t.text :introduction,             null: false
-     t.boolean :is_active,             null: false, default: TRUE
+     t.boolean :is_active,             null: false, default: true
       t.timestamps
     end
   end
