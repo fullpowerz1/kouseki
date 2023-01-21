37547219a3d8b8f455fd078979167041ada17812
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2023-01-21 08:27:43 +0000

    igunister

--- db/migrate/20221225021130_create_tagmaps.rb
+++ db/migrate/20221225021130_create_tagmaps.rb
@@ -1,8 +1,8 @@
 class CreateTagmaps < ActiveRecord::Migration[6.1]
   def change
     create_table :tagmaps do |t|
-      t.references :ore, null: false, foreign_key: true
-      t.references :tag, null: false, foreign_key: true
+      t.integer :ore_id, null: false
+      t.integer :tag_id, null: false
 
       t.timestamps
     end
