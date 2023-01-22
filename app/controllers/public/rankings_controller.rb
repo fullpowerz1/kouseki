ae03329fe42cd2a38e6702f709ad15b3d98ce425
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2023-01-22 13:59:50 +0000

    danmari

--- app/controllers/public/rankings_controller.rb
+++ app/controllers/public/rankings_controller.rb
@@ -19,32 +19,14 @@ class Public::RankingsController < ApplicationController
   end
 
   def confirm
-    @ranking = Ranking.new(ranking_params)
-    #byebug
-    # @ranking.rank = params[:ranking][:rank] #newの所で渡しているからいらない。
+    @ranking = Ranking.new(ranking_params)。
     @ranking.customer_id = current_customer.id
-    #@ranking.amount = 1
     @ore = Ore.find(ranking_params[:ore_id])
-    # if @ranking.save!
-     #  redirect_to rankings_complete_path
-    # 手持ちの鉱石から選択
-    # if params[:ranking][:select_material] == "0" ##どれを選択したのかをparamsから読み取る。
-    #   @ranking.ore_id = current_customer.ore ##ランク付けする鉱石を反映
-    #   @ranking.amount =current_customer.amount ##顧客がランク付けしたい鉱石の数
-    # render "confirm"
-    # else
-      # render :new
-    # end
     # # 装飾品を選択
-
     if params[:ranking][:select_material] == "1" ##どれを選択したのかをparamsから読み取る。
       @decoration = current_customer.decorations.new ##装飾品を新規登録
       @decoration.amulet = params[:ranking][:amulet] #装飾品の種類を登録
       @decoration.save# if @decoration.save
-      #   @ranking.type = @decoration.type
-    #   else
-    #     render "new"
-    #   end
     end
   end
 
