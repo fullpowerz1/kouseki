b62dc8cc0cf1d5a761d3d60512de319e151cd24c
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2023-01-27 13:37:20 +0000

    tyon

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,5 +1,5 @@
 class Admin::HomesController < ApplicationController
   def top
-    @rankings = Ranking.page(params[:page])
+    @rankings = Ranking.page(params[:page]).per(8).order('created_at DESC')
   end
 end
