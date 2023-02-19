2a9386bd9dcbd3f01d9ad0338c6c9c9e016b902b
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2023-02-19 08:06:51 +0000

    jin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-# 後々アクセス権限を加える。
+# 後々アクセス権限を加える。そのうち
 
 # ユーザー管理用のbefore_action
   before_action :configure_permitted_parameters, if: :devise_controller?
