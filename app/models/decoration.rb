d888acd7f90cdaf504e977a77084d0e9d76c22eb
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2023-02-21 14:41:53 +0000

    igunister

--- app/models/decoration.rb
+++ app/models/decoration.rb
@@ -1,3 +1,8 @@
 class Decoration < ApplicationRecord
      belongs_to :customer
+
+     # バリデーション
+     validates :ore, presence: true, length: { minimum: 3 }
+     validates :amulet, presence: true
+     validates :nickname, presence: true, uniqueness: true, length: { minimum: 3 }
 end
