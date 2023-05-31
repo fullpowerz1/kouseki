a9f1d2912f5be90794d977a93f3c30191cad75b0
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2023-05-31 00:57:02 +0000

    gojira

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-# 会員用1です。それから改善します。
+# 会員用1です。それから改善する予定です。
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
