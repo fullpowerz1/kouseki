d09081f14d31e8202fc5f4a8c941ef6678e34f7f
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2023-09-23 08:55:32 +0000

    tyon

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-# 会員用1です。それさえあれ。
+# 会員用1です。それさえ
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
