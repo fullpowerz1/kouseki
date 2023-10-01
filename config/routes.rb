9a529495c589acc7db57fd0204dec90c68ddb5fc
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2023-10-01 08:18:45 +0000

    an

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-# 会員用1です。
+# 会員用1です。もしくは。
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
